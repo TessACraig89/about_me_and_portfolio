b3ad05f6aea482342352ad8d5fd1d6c39a49e28a
Tess Craig <tessashleycraig89@gmail.com>
2017-12-18 09:03:22 -0600

    Commit 19 - adds jquery hover and mouseleave color change for all links

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
+$(".aboutMeLink").hover(function() {
+  $( this ).css("color", "red");
+});
 
+$( ".aboutMeLink").mouseleave(function() {
+  $( this ).css("color", "teal");
+});
+
+$(".portfolioLink").hover(function() {
+  $( this ).css("color", "red");
+});
+
+$(".portfolioLink").mouseleave(function() {
+  $( this ).css("color", "teal");
+});
+
+$(".linkedinAbout").hover(function() {
+  $( this ).css("color", "red");
+});
+
+$(".linkedinAbout").mouseleave(function() {
+  $( this ).css("color", "black");
+});
+
+$(".githubAbout").hover(function() {
+  $( this ).css("color", "red");
+});
+
+$(".githubAbout").mouseleave(function() {
+  $( this ).css("color", "black");
+});
+
+$(".linkedinPortfolio").hover(function() {
+  $( this ).css("color", "red");
+});
+
+$(".linkedinPortfolio").mouseleave(function() {
+  $( this ).css("color", "black");
+});
+
+$(".githubPortfolio").hover(function() {
+  $( this ).css("color", "red");
+});
+
+$(".githubPortfolio").mouseleave(function() {
+  $( this ).css("color", "black");
+});
